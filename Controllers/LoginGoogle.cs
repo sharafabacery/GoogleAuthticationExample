217c6f41ae2606b3a6a12cf34b67095a7a71a56a
sharaf eldien ehab <52586357+sharafabacery@users.noreply.github.com>
2024-09-25 01:25:45 +0300

    Construct DI

--- Controllers/LoginGoogle.cs
+++ Controllers/LoginGoogle.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GoogleAuthticationExample.Repositories.Interfance;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GoogleAuthticationExample.Controllers
 {
     public class LoginGoogle : Controller
     {
+        private readonly ILoginGoogleService loginGoogleService;
+
+        public LoginGoogle(ILoginGoogleService loginGoogleService)
+        {
+            this.loginGoogleService = loginGoogleService;
+        }
         public IActionResult Index()
         {
             return View();
