324052caa1413aa13af9b18280026826cd8c85c8
sharaf eldien ehab <52586357+sharafabacery@users.noreply.github.com>
2024-09-25 01:16:51 +0300

    Create interface to hold login google api

---
+++ Controllers/LoginGoogle.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GoogleAuthticationExample.Controllers
+{
+    public class LoginGoogle : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Repositories/Implementaion/LoginGoogleService.cs
@@ -0,0 +1,26 @@
+﻿using GoogleAuthticationExample.Repositories.Interfance;
+using Microsoft.AspNetCore.Identity;
+
+namespace GoogleAuthticationExample.Repositories.Implementaion
+{
+    public class LoginGoogleService : ILoginGoogleService
+    {
+        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly UserManager<IdentityUser> userManager;
+
+        public LoginGoogleService(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+        }
+        public Task<bool> LoginExtnal(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RegisterExtnal(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
